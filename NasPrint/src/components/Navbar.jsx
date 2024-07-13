6ac800c34b60e56b8f0aea7e5fb19d59daff2ea2
AbdelDjalilH <adj.hamzaoui@gmail.com>
2024-07-13 23:40:53 +0200

    fix css navbar

--- NasPrint/src/components/Navbar.jsx
+++ NasPrint/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import logoNas from "../assets/navImages/logoNas.png";
+import "../styles/navbar.css";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="navbar-container">
       <Link to="/">
         <img src={logoNas} alt="" />
       </Link>
